798dc7c0f98dae8b840cd3462093c0b2dd7886d2
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-07-23 14:34:52 -0500

    some bar changes

--- main.lua
+++ main.lua
@@ -8,12 +8,18 @@ function love.load()
 
     bar={w=150,h=8,x=1,y=1,
     sel={x=0,spd=120,w=3,dx=0},
-        sweet={x=0,w=16,dx=0,dw=16}
+        sweet={x=0,w=16,dx=0,dw=16,hp=1}
     }
     bar.sel.x=bar.w-3
     bar.x=conf.gW/2-bar.w/2
     bar.y=conf.gH-8-3
     score=0
+    combo=0
+    colors={
+        {0,1,1},
+        {1,1,0},
+        {1,0,0}
+    }
 end
 
 local function lerp(a, b, t)
@@ -23,14 +29,23 @@ end
 function love.update(dt)
     input:update()
     bar.sel.x=bar.sel.x-bar.sel.spd*dt
-    if (input:pressed("a") and bar.sweet.x<=(bar.sel.x+bar.sel.w) and bar.sweet.x+bar.sweet.w>=bar.sel.x) then
+    local i=input:pressed("a")
+    local c=bar.sweet.x<=(bar.sel.x+bar.sel.w) and bar.sweet.x+bar.sweet.w>=bar.sel.x
+    if i then
         bar.sel.x=bar.w-3
         bar.sweet.x=math.random(0,bar.w-56)
         bar.sweet.w=math.random(10,32)
-        score=score+1
+        --bar.sweet.hp=math.random(1,3)
+        if c then
+            score=score+1
+            combo=combo+1
+        else
+            combo=0
+        end
     end
     if bar.sel.x<0 then
         bar.sel.x=bar.sel.x+bar.w-3
+        combo=0
     end
     bar.sweet.dx=lerp(bar.sweet.dx,bar.sweet.x,10*dt)
     bar.sweet.dw=lerp(bar.sweet.dw,bar.sweet.w,8*dt)
@@ -46,5 +61,6 @@ function love.draw()
         lg.rectangle("fill",bar.x+bar.sel.x,bar.y,bar.sel.w,bar.h)
         lg.setColor(1,1,1)
         lg.print("Score: "..score,1,1)
+        lg.print("Combo x "..combo,1,13)
     endDraw()
 end
